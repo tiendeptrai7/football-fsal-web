d986776702b0df0769dc5fc6e017fae586fb553a
Tien Phan <77587642+tiendeptrai7@users.noreply.github.com>
2024-10-12 15:31:17 +0700

    fix menu

--- src/app/shared/layouts/main-layout/main-layout.component.ts
+++ src/app/shared/layouts/main-layout/main-layout.component.ts
@@ -1,21 +1,50 @@
-import { Component, Injector, OnInit } from '@angular/core';
+import {
+  Component,
+  Injector,
+  OnDestroy,
+  OnInit,
+  Renderer2,
+} from '@angular/core';
 import { UserService } from '@services/user.service';
 import { AppBaseComponent } from '@shared/app.base.component';
 
 @Component({
   templateUrl: './main-layout.component.html',
 })
-export class MainLayoutComponent extends AppBaseComponent implements OnInit {
+export class MainLayoutComponent
+  extends AppBaseComponent
+  implements OnInit, OnDestroy
+{
+  private scrollListener!: () => void;
+
   isCollapsed = false;
+  scrolling = false;
 
   constructor(
     injector: Injector,
-    private readonly userService: UserService
+    private readonly userService: UserService,
+    private renderer: Renderer2
   ) {
     super(injector);
   }
+  ngOnDestroy(): void {
+    if (this.scrollListener) {
+      this.scrollListener();
+    }
+  }
 
   ngOnInit(): void {
+    this.scrollListener = this.renderer.listen('window', 'scroll', () => {
+      const scrollTop = window.scrollY || document.documentElement.scrollTop;
+      const sticker = document.getElementById('sticker');
+      if (scrollTop > 50) {
+        console.log('sticker', sticker);
+        sticker?.classList.add('is-sticky');
+      } else {
+        sticker?.classList.remove('is-sticky');
+      }
+    });
+
     if (localStorage.getItem('is_change_password')) {
       return this.redirect('/change-password');
     }
